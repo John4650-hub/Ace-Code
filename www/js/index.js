2fc3473288073749951b751516531f0d76547021
John Delvin <johndelvin51@gmail.com>
2022-11-24 03:46:03 +0300

    Build apk and release apk

--- www/js/index.js
+++ www/js/index.js
@@ -18,4 +18,13 @@ editor.session.setMode("ace/mode/python");
 
 const aline = document.getElementById("air-line");
 aline.innerHTML = `<b>${editor.getSession().getMode().$id}</b>`;
-
+// App request permissiom for storage
+window.requestFielSystem(LocalFileSystem.PERSISTENT,0,function (fs){
+  console.log("file system open: "+fs.name);
+  fs.root.getFile("newPersistentFile.txt",{create:true,exclusive:false}, function (fileEntry){
+  console.log("File Entry is "+ fileEntry.isFile.toString());
+  fileEntry.name = "Sample.py";
+  writeFile(fileEntry,null);
+  },
+onErrorCreateFile);
+},onErrorLoadFs);
