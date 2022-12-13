a580fb3ea91c31cb430df46af71bf9ddb7423e08
johndelvin51@gmail.com <johndelvin51@gmail.com>
2022-12-13 21:00:24 +0300

    Now it's possible to write to both sdcard and external storage

--- www/js/components/fileSys.js
+++ www/js/components/fileSys.js
@@ -2,57 +2,43 @@ import { OPENFS, SAVEFS, sendData } from "../app.js";
 // import cordova from "../cordova.js";
 import { editor } from "../index.js";
 export function onDeviceReady() {
+window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fs) {
 
-  window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function(dirEntry) {
-    let isAppend = false;
-    console.log(dirEntry);
-    createFile(dirEntry, "fileToAppend.txt", isAppend);
-  }, ()=>{console.log("failed to load file system");});
+  console.log('file system open: ' + fs.name);
+  console.log(fs.root);
+  fs.root.getFile("sample.txt", { create: true, exclusive: false }, function(fileEntry) {
+    console.log(` fileEntrt = ${fileEntry}`);
+    console.log("fileEntry is file?" + fileEntry.isFile.toString());
+    console.log(fileEntry.name);
+    console.log(fileEntry.fullPath);
+    writeFile(fileEntry, null);
+
+  }, ()=>{"tt"});
+
+}, ()=>{console.log('eer');});
+  
+  
 }
-function createFile(dirEntry, fileName, isAppend) {
-  // Creates a new file or returns the file if it already exists.
-  dirEntry.getFile(fileName, { create: true, exclusive: false }, function(fileEntry) {
 
-    writeFile(fileEntry, null, isAppend);
-
-  }, ()=>{console.log("failed to create file");});
-
-}
-function writeFile(fileEntry, dataObj, isAppend) {
+function writeFile(fileEntry, dataObj) {
   // Create a FileWriter object for our FileEntry (log.txt).
   fileEntry.createWriter(function(fileWriter) {
 
     fileWriter.onwriteend = function() {
-      console.log("Successful file read...");
-      readFile(fileEntry);
+      console.log("Successful file write...");
+      // readFile(fileEntry);
     };
 
     fileWriter.onerror = function(e) {
-      console.log("Failed file read: " + e.toString());
+      console.log("Failed file write: " + e.toString());
     };
 
-    // If we are appending data to file, go to the end of the file.
-    if (isAppend) {
-      try {
-        fileWriter.seek(fileWriter.length);
-      }
-      catch (e) {
-        console.log("file doesn't exist!");
-      }
+    // If data object is not passed in,
+    // create a new Blob instead.
+    if (!dataObj) {
+      dataObj = new Blob(['some file data'], { type: 'text/plain' });
     }
+
     fileWriter.write(dataObj);
   });
-}
-function readFile(fileEntry) {
-
-  fileEntry.file(function(file) {
-    var reader = new FileReader();
-
-    reader.onloadend = function() {
-      console.log("Successful file read: " + this.result);
-    };
-
-    reader.readAsText(file);
-
-  }, ()=>{console.log("failed to read file");});
 }
